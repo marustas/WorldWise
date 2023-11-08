87b8311362dc0bd985aa127683aefcf04be272cd
Stanislau Marudau <marudov.stas@gmail.com>
2023-11-08 20:54:15 +0100

    Added Login route

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,15 @@ import Homepage from "./pages/Homepage";
 import Pricing from "./pages/Pricing";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
+import Login from "./pages//Login";
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homepage />} />
-        <Route path="product" element={<Product />} />
         <Route path="pricing" element={<Pricing />} />
+        <Route path="product" element={<Product />} />
+        <Route path="login" element={<Login />} />
         <Route path="*" element={<PageNotFound />} />
         <Route path="app" element={<AppLayout />} />
       </Routes>
