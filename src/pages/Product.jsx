e2c132f67516586aa4ebfa0535ff2b6ce8168064
Stanislau Marudau <marudov.stas@gmail.com>
2023-11-08 20:52:05 +0100

    Added navigation to each page

--- src/pages/Product.jsx
+++ src/pages/Product.jsx
@@ -1,8 +1,10 @@
 import styles from "./Product.module.css";
+import PageNav from "../components/PageNav";
 
 export default function Product() {
   return (
     <main className={styles.product}>
+      <PageNav />
       <section>
         <img
           src="img-1.jpg"
