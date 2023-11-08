5ace88a2a4737d73952476ea6acfcd7e3dc0a647
Stanislau Marudau <marudov.stas@gmail.com>
2023-11-08 21:35:56 +0100

    Improved functionality of the App navigation

--- src/components/AppNav.jsx
+++ src/components/AppNav.jsx
@@ -1,6 +1,18 @@
+import { NavLink } from "react-router-dom";
 import { nav } from "./AppNav.module.css";
 const AppNav = () => {
-  return <nav className={nav}>App navigation</nav>;
+  return (
+    <nav className={nav}>
+      <ul>
+        <li>
+          <NavLink to="cities">Cities</NavLink>
+        </li>
+        <li>
+          <NavLink to="countries">Countries</NavLink>
+        </li>
+      </ul>
+    </nav>
+  );
 };
 
 export default AppNav;
