50f60f610e947305c1545eb0a1fb6ac696974355
Stanislau Marudau <marudov.stas@gmail.com>
2023-11-08 21:26:53 +0100

    Fixed the issue with the Logo element

--- src/App.jsx
+++ src/App.jsx
@@ -10,12 +10,17 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Homepage />} />
+        <Route index element={<Homepage />} />
         <Route path="pricing" element={<Pricing />} />
         <Route path="product" element={<Product />} />
         <Route path="login" element={<Login />} />
         <Route path="*" element={<PageNotFound />} />
-        <Route path="app" element={<AppLayout />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route index element={<p>List of cities</p>} />
+          <Route path="cities" element={<p>List of cities</p>} />
+          <Route path="countries" element={<p>List of countries</p>} />
+          <Route path="form" element={<p>Form</p>} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -2,12 +2,13 @@ import { sidebar } from "./Sidebar.module.css";
 import Logo from "./Logo";
 import AppNav from "./AppNav";
 import Footer from "./Footer";
+import { Outlet } from "react-router-dom";
 const SideBar = () => {
   return (
     <div className={sidebar}>
       <Logo />
       <AppNav />
-      <p>City lists</p>
+      <Outlet />
       <Footer />
     </div>
   );
