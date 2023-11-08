79f8c5bb6c480800fc4b846f227e498a7c4473af
Stanislau Marudau <marudov.stas@gmail.com>
2023-11-08 20:02:59 +0100

    Changed to NavLink

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Product from "./pages/product";
+import Product from "./pages/Product";
 import Homepage from "./pages/Homepage";
 import Pricing from "./pages/Pricing";
 import PageNotFound from "./pages/PageNotFound";
--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -1,17 +1,17 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 const PageNav = () => {
   return (
     <nav>
       <ul>
         <li>
-          <Link to="/">Home</Link>
+          <NavLink to="/">Home</NavLink>
         </li>
         <li>
-          <Link to="/pricing">Pricing</Link>
+          <NavLink to="/pricing">Pricing</NavLink>
         </li>
         <li>
-          <Link to="/product">Product</Link>
+          <NavLink to="/product">Product</NavLink>
         </li>
       </ul>
     </nav>
