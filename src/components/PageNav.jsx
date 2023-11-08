cf6449b72a158f09c210913e14a3a3471514adf1
Stanislau Marudau <marudov.stas@gmail.com>
2023-11-08 20:49:13 +0100

    Added Logo component

--- src/components/PageNav.jsx
+++ src/components/PageNav.jsx
@@ -1,18 +1,19 @@
 import { NavLink } from "react-router-dom";
 import { nav } from "./PageNav.module.css";
-
+import Logo from "./Logo";
 const PageNav = () => {
   return (
     <nav className={nav}>
+      <Logo />
       <ul>
-        <li>
-          <NavLink to="/">Home</NavLink>
-        </li>
         <li>
           <NavLink to="/pricing">Pricing</NavLink>
         </li>
         <li>
           <NavLink to="/product">Product</NavLink>
+        </li>
+        <li>
+          <NavLink to="/login">Login</NavLink>
         </li>
       </ul>
     </nav>
