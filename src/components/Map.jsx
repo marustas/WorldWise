2f92a4993a73be93bc88a468902503aa86b26ce9
Stanislau Marudau <marudov.stas@gmail.com>
2023-11-15 16:04:05 +0100

    Implemented leaflet into the map component

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,15 +1,41 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
-import { mapContainer } from "./Map.module.css";
+import { mapContainer, map } from "./Map.module.css";
+import { MapContainer, Popup, TileLayer, Marker } from "react-leaflet";
+import { useState } from "react";
+import { useCities } from "../context/CitiesContext";
+
 const Map = () => {
   const navigate = useNavigate();
+  const { cities } = useCities();
   const [searchParams, setSearchParams] = useSearchParams();
   const lat = searchParams.get("lat");
   const lng = searchParams.get("lng");
+  const [position, setPosition] = useState([40, 0]);
 
   return (
-    <div className={mapContainer} onClick={() => navigate("form")}>
-      <h1>Latitude: {lat}</h1>
-      <h1>Longitude: {lng}</h1>
+    <div className={mapContainer}>
+      <MapContainer
+        className={map}
+        center={position}
+        zoom={13}
+        scrollWheelZoom={true}
+      >
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
+        />
+        {cities.map((city) => (
+          <Marker
+            position={[city.position.lat, city.position.lng]}
+            key={city.id}
+          >
+            <Popup>
+              <span>{city.emoji}</span>
+              <span>{city.cityName}</span>
+            </Popup>
+          </Marker>
+        ))}
+      </MapContainer>
     </div>
   );
 };
