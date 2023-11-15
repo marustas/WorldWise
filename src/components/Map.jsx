5ede7eb2f220eb5a9bbf29d98711515cc78ba016
Stanislau Marudau <marudov.stas@gmail.com>
2023-11-15 16:48:24 +0100

    Implemented map tracking of the current city

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,23 +1,32 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
 import { mapContainer, map } from "./Map.module.css";
-import { MapContainer, Popup, TileLayer, Marker } from "react-leaflet";
-import { useState } from "react";
+import { MapContainer, Popup, TileLayer, Marker, useMap } from "react-leaflet";
+import { useState, useEffect } from "react";
 import { useCities } from "../context/CitiesContext";
 
 const Map = () => {
   const navigate = useNavigate();
+  const [searchParams] = useSearchParams();
   const { cities } = useCities();
-  const [searchParams, setSearchParams] = useSearchParams();
   const lat = searchParams.get("lat");
   const lng = searchParams.get("lng");
   const [position, setPosition] = useState([40, 0]);
+
+  useEffect(
+    function () {
+      if (lat && lng) {
+        setPosition([lat, lng]);
+      }
+    },
+    [lat, lng]
+  );
 
   return (
     <div className={mapContainer}>
       <MapContainer
         className={map}
         center={position}
-        zoom={13}
+        zoom={6}
         scrollWheelZoom={true}
       >
         <TileLayer
@@ -35,9 +44,16 @@ const Map = () => {
             </Popup>
           </Marker>
         ))}
+        <ChangeCenter position={position} />
       </MapContainer>
     </div>
   );
 };
+
+function ChangeCenter({ position }) {
+  const map = useMap();
+  map.setView(position);
+  return null;
+}
 
 export default Map;
